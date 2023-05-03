b6cf8b354f009f1ca849565e3b042dd7ba9d74c6
Kataplan <dcatalanmarchese@gmail.com>
2023-05-03 16:42:49 -0400

    Update prep.py

--- prep.py
+++ prep.py
@@ -1,39 +1,53 @@
 import pandas as pd
 import numpy as np
-import utility as ut
 
 # Save Data : training and testing
 
 
-def save_data():
+def save_data(data):
+    np.savetxt("dtrain_xe.csv", data['xe'], delimiter=",", fmt='%f')
+    np.savetxt("dtrain_ye.csv", data['ye'], delimiter=",", fmt='%d')
+    np.savetxt("dtest_xv.csv", data['xv'], delimiter=",", fmt='%f')
+    np.savetxt("dtest_yv.csv", data['yv'], delimiter="," , fmt='%d')
 
     return
 
 
 # Binary Label
-def binary_label():
-
-    return
+def binary_label(y):
+    y = y.astype(int)
+    max_value = np.max(y)
+    binary_array = np.eye(max_value, dtype=int)
+    return binary_array[y - 1]
 
 
 # Load data csv
-def load_data_csv():
+def load_data_csv(Param):
+    d_train = np.genfromtxt('train.csv', delimiter=',')
+    d_test = np.genfromtxt('test.csv', delimiter=',')
+    data = dict()
+    data['xe'] = d_train[:, :-1]
+    print(data['xe'].shape)
+    data['ye'] = binary_label(d_train[:, -1])
+    data['xv'] = d_test[:, :-1]
+    data['yv'] = binary_label(d_test[:, -1])
 
-    return ()
+    return data
 
 
 # Configuration of the SAEs
 def load_config():
-    par = np.genfromtxt('cnf_sae.csv', delimiter=',')
+    par_sae = np.genfromtxt('cnf_sae.csv', delimiter=',')
+    par_sft = np.genfromtxt('cnf_sae.csv', delimiter=',')
 
     return (par_sae, par_sft)
 
 
 # Beginning ...
 def main():
-    Param = ut.load_cnf()
-    Data = load_data_csv()
-    save_data()
+    Param = load_config()
+    Data = load_data_csv(Param)
+    save_data(Data)
 
 
 if __name__ == '__main__':

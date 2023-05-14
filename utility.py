31494365ae46d37cf9c0ad8e393d440bf6c16b28
Kataplan <dcatalanmarchese@gmail.com>
2023-05-14 19:40:46 -0400

    Update utility

--- utility.py
+++ utility.py
@@ -11,27 +11,60 @@ def iniW(next, prev):
     w = w*2*r-r
     return (w)
 
+
 # STEP 1: Feed-forward of AE
-def ae_forward(x, w1, w2):
-    return (a)
+def ae_forward(x, w1, w2, act_func):
+
+    act_1 = act_function(np.dot(x,w1.T), act_func)
+    act_2 = np.dot(w2, act_1)
+    return [x, act_1, act_2.T]
+
 
 # Calculate Pseudo-inverse
 def pinv_ae(x, w1, C):
 
     return (w2)
 
+
 # STEP 2: Feed-Backward for AE
-def gradW1(a, w2):
+def gradW1(a, w1, w2, act_f):
+
     e = a[2]-a[0]
     Cost = np.sum(np.sum(e**2))/(2*e.shape[1])
+    z1 = np.dot(w1, a[0].T)
+    del_2 = np.multiply(e, 1)
+    xd = np.dot(derivate_act(z1, act_f), a[0])
+    gW = np.multiply(xd.T, np.dot(w2.T, del_2.T))
+    return (gW, Cost)
 
-    return (gW1, Cost)
+
+# Update W and V
+def updW1_sgdm(w, V, gW, param):
+
+    return
+
+
+# Sort x y random
+def sort_data_ramdom(X, Y):
+    XY = np.concatenate((X, Y), axis=1)
+    np.random.shuffle(XY)
+    X_new, Y_new = np.split(XY, [X.shape[1]], axis=1)
+    return X_new, Y_new
+
 
 # Update AE's weight via RMSprop
 def updW1_rmsprop(w, v, gw, mu):
     beta, eps = 0.9, 1e-8
+    v_i = np.dot(beta * v + (1 - beta), gw**2).T
+    print("v_i = ",v_i.shape)
+    gRMS = np.dot(gw, 1/(np.sqrt(v_i+eps))).T
+    print("gRms = ",gRMS.shape)
+    xd =  mu*gRMS
+    
+    w_i = w-xd
+    print(w_i.shape)
+    return (w_i, v_i.T)
 
-    return (w, v)
 
 # Update Softmax's weight via RMSprop
 def updW_sft_rmsprop(w, v, gw, mu):
@@ -39,16 +72,19 @@ def updW_sft_rmsprop(w, v, gw, mu):
 
     return (w, v)
 
+
 # Softmax's gradient
 def gradW_softmax(x, y, a):
     ya = y*np.log(a)
 
     return (gW, Cost)
 
+
 def load_config():
     par_sae = np.genfromtxt('cnf_sae.csv', delimiter=',')
     par_sft = np.genfromtxt('cnf_sae.csv', delimiter=',')
     return (par_sae, par_sft)
+
 
 # Calculate Softmax
 def softmax(z):
@@ -61,67 +97,84 @@ def save_w_dl(W, Ws, cost):
     return
 
 
-#Activation function
-def act_function(w,X,function_number):
-    z = np.dot(w.T, X)
-    if(function_number==1):
+# Activation function
+def act_function(z, function_number):
+    
+    if (function_number == 1):
         h_z = ReLu_function(z).T
-    if(function_number==2):
+    if (function_number == 2):
         h_z = L_ReLu_function(z).T
-    if(function_number==3):
-        h_z = ELU_function(z).T 
-    if(function_number==4):
+    if (function_number == 3):
+        h_z = ELU_function(z).T
+    if (function_number == 4):
         h_z = SELU_function(z).T
-    if(function_number==5):
+    if (function_number == 5):
         h_z = sigmoidal_function(z).T
-    return(h_z)
+    return (h_z)
 
-def derivate_act(z,function_number):
-    if(function_number==1):h_z = d_ReLu_function(z)
-    elif(function_number==2):h_z = d_L_ReLu_function(z)
-    elif(function_number==3):h_z = d_ELU_function(z)
-    elif(function_number==4):h_z = d_SELU_function(z)
-    elif(function_number==5):h_z = d_sigmoidal_function(z)
-    return(h_z)
 
-def output_activation(v,h):
+def derivate_act(z, function_number):
+    if (function_number == 1):
+        h_z = d_ReLu_function(z)
+    elif (function_number == 2):
+        h_z = d_L_ReLu_function(z)
+    elif (function_number == 3):
+        h_z = d_ELU_function(z)
+    elif (function_number == 4):
+        h_z = d_SELU_function(z)
+    elif (function_number == 5):
+        h_z = d_sigmoidal_function(z)
+    return (h_z)
+
+
+def output_activation(v, h):
     z = np.dot(v, h.T)
     y = 1/(1+np.exp(-z))
     return y.T
 
+
 def ReLu_function(x):
-    return np.where(x>0,x,0)
+    return np.where(x > 0, x, 0)
+
 
 def L_ReLu_function(x):
-    return np.where(x<0,0.01*x,x)
+    return np.where(x < 0, 0.01*x, x)
+
 
 def ELU_function(x):
     a = 1.6732
-    return np.where(x>0,x,a*(np.exp(x)-1))
+    return np.where(x > 0, x, a*(np.exp(x)-1))
+
 
 def SELU_function(x):
     a = 1.6732
-    lam =1.0507
-    return np.where(x>0,x*lam,a*(np.exp(x)-1))
-      
+    lam = 1.0507
+    return np.where(x > 0, x*lam, a*(np.exp(x)-1))
+
+
 def sigmoidal_function(z):
     return 1.0/(1.0+np.exp(-z))
 
+
 def d_ReLu_function(x):
-    return np.maximum(0,x)
+    return np.maximum(0, x)
+
 
 def d_L_ReLu_function(x):
-    return np.where(x<0,0.01*x,x)
+    return np.where(x < 0, 0.01*x, x)
+
 
 def d_ELU_function(x):
     a = 1.6732
-    return np.where(x>0,1, a*np.exp(x))
+    return np.where(x > 0, 1, a*np.exp(x))
+
 
 def d_SELU_function(x):
-    lam = 1.0507; 
+    lam = 1.0507
     a = 1.6732
-    return np.where(x>0, 1, a*np.exp(x))*lam
-      
+    return np.where(x > 0, 1, a*np.exp(x))*lam
+
+
 def d_sigmoidal_function(z):
-    return (np.multiply(1/(1+np.exp(-z)),1-(1/(1+np.exp(-z))))).T
-#-----------------------------------------------------------------------
+    return (np.multiply(1/(1+np.exp(-z)), 1-(1/(1+np.exp(-z))))).T
+# -----------------------------------------------------------------------
